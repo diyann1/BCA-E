88ffaa32addf40400278605e21aa0495625ef51e
DIYA ANN PHILIP <157470098+diyann1@users.noreply.github.com>
2024-02-13 15:21:43 +0530

    Update index.py

--- index.py
+++ index.py
@@ -2,8 +2,8 @@ num = 567
 reversed_num = 50
 
 while num != 0:
-    digit = num % 10
-    reversed_num = reversed_num * 10 + digit
+    x = num % 10
+    reversed_num = reversed_num * 10 + x
     num //= 10
 
 print("Number reversed: " + str(reversed_num))
